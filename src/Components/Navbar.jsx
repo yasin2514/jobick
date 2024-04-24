3abe26626f5c694b3ff4448eef499d811389d4c8
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-04-24 23:54:57 +0600

    short navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -62,9 +62,8 @@ const Navbar = ({ showFull }) => {
       {showFull && <NavbarBrand></NavbarBrand>}
       {!showFull && (
         <div className="space-y-4 mt-5">
-
           {/* dropdown-1 */}
-          <div className="dropdown dropdown-hover">
+          <div className="dropdown dropdown-hover w-full">
             <div tabIndex={0} role="button">
               <button className="bg-gray-300 p-1 flex flex-col items-center gap-3 w-full py-3 rounded-md hover:bg-gray-400">
                 <FaFlag className="text-red-600"></FaFlag>
@@ -77,29 +76,67 @@ const Navbar = ({ showFull }) => {
               tabIndex={0}
               className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-md w-40"
             >
-              <ul className=" custom-ul mt-1  ">
-                <NavLink
-                  to="/"
-                  className={({ isActive }) =>
-                    isActive
-                      ? "flex items-center justify-start  px-2 text-[14px] gap-3 font-semibold text-red-600"
-                      : "flex items-center  justify-start  px-2  text-[14px] gap-3"
-                  }
-                >
-                  <FaMinus className=" fa-minus"></FaMinus>{" "}
-                  <span className="custom-span">New Job</span>
-                </NavLink>
+              <ul className=" custom-ul mt-1  custom-div-2">
+                <li>
+                  <NavLink
+                    to="/d"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center justify-start  px-2 text-[14px] gap-3 font-semibold text-red-600"
+                        : "flex items-center  justify-start  px-2  text-[14px] gap-3"
+                    }
+                  >
+                    <FaMinus className=" fa-minus custom-text4"></FaMinus>{" "}
+                    <span className="custom-span custom-text4">New Job</span>
+                  </NavLink>
+                </li>
               </ul>
             </ul>
           </div>
 
           {/* dropdown two */}
-          <button className="bg-gray-300 p-1 flex flex-col items-center gap-3 w-full py-4 rounded-md hover:bg-gray-400">
-            <FaInfoCircle className="text-red-600"></FaInfoCircle>
-          </button>
+          <div className="dropdown dropdown-hover w-full">
+            <div tabIndex={0} role="button">
+              <button className="bg-gray-300 p-1 flex flex-col items-center gap-3 w-full py-4 rounded-md hover:bg-gray-400">
+                <FaInfoCircle className="text-red-600"></FaInfoCircle>
+              </button>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-md w-40"
+            >
+              <ul className="  mt-1  ">
+                <li className="custom-ul custom-div-2">
+                  <NavLink
+                    to="/read"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center justify-start  px-2 text-[14px] gap-3 font-semibold text-red-600"
+                        : "flex items-center  justify-start  px-2  text-[14px] gap-3"
+                    }
+                  >
+                    <FaMinus className=" fa-minus custom-text4"></FaMinus>{" "}
+                    <span className="custom-span custom-text4">Read</span>
+                  </NavLink>
+                </li>
+                <li className="custom-ul custom-div-2">
+                  <NavLink
+                    to="/shop"
+                    className={({ isActive }) =>
+                      isActive
+                        ? "flex items-center justify-start  px-2 text-[14px] gap-3 font-semibold text-red-600"
+                        : "flex items-center  justify-start  px-2  text-[14px] gap-3"
+                    }
+                  >
+                    <FaMinus className=" fa-minus custom-text4"></FaMinus>{" "}
+                    <span className="custom-span custom-text4">Shop</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </ul>
+          </div>
         </div>
       )}
-
     </>
   );
 };
