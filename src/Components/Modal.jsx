432db6efbfa2411145c3c17719f33e3cd4749087
yasin2514 <yasinkhanrabbi1999@gmail.com>
2024-04-25 11:27:11 +0600

    modal updatedB

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 const Modal = ({modalId}) => {
   return (
@@ -6,12 +5,22 @@ const Modal = ({modalId}) => {
       <dialog id={modalId} className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg">Job title</h3>
-          <p className="py-4">
-            Add a job text details
-          </p>
+          <form className="space-y-3 my-3">
+            <input
+              type="text"
+              placeholder="Type here Job Name"
+              className="input input-bordered w-full "
+            />
+            <textarea
+              className="textarea textarea-bordered w-full"
+              placeholder="Type Here Job Description"
+            ></textarea>
+          </form>
           <div className="modal-action">
             <form method="dialog">
-              <button className="btn btn-success text-white">Save Changes</button>
+              <button className="btn btn-success text-white">
+                Save Changes
+              </button>
               <button className="btn ms-5 btn-error text-white">Close</button>
             </form>
           </div>
--- src/Components/Notifiction.jsx
+++ src/Components/Notifiction.jsx
@@ -1,72 +1,68 @@
-import React from 'react';
-import { FaRegUser } from 'react-icons/fa';
-import { IoIosLogOut } from 'react-icons/io';
-import { IoNotificationsOutline } from 'react-icons/io5';
-import { MdOutlineEmail } from 'react-icons/md';
-import { NavLink } from 'react-router-dom';
+import { IoNotificationsOutline } from "react-icons/io5";
+import { NavLink } from "react-router-dom";
 
 const Notifiction = () => {
-    return (
-      <>
-        <ul
-          tabIndex={0}
-          className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-        >
-          <li>
-            <NavLink
-              to="/profile"
-              className={({ isActive }) =>
-                isActive
-                  ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
-                  : "flex items-center  text-[16px] gap-3 font-semibold"
-              }
-            >
-              <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
-              Notifiction text-1
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/inbox"
-              className={({ isActive }) =>
-                isActive
-                  ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
-                  : "flex items-center  text-[16px] gap-3 font-semibold"
-              }
-            >
-              <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
-              Notifiction text-1
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/logout"
-              className={({ isActive }) =>
-                isActive
-                  ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
-                  : "flex items-center  text-[16px] gap-3 font-semibold"
-              }
-            >
-              <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
-              Notifiction text-1
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/logout"
-              className={({ isActive }) =>
-                isActive
-                  ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
-                  : "flex items-center  text-[16px] gap-3 font-semibold"
-              }
-            >
-              <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
-              Notifiction text-1
-            </NavLink>
-          </li>
-        </ul>
-      </>
-    );
+  return (
+    <>
+      <ul
+        tabIndex={0}
+        className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+      >
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
+                : "flex items-center  text-[16px] gap-3 font-semibold"
+            }
+          >
+            <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
+            Notifiction text-1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/inbox"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
+                : "flex items-center  text-[16px] gap-3 font-semibold"
+            }
+          >
+            <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
+            Notifiction text-1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/logout"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
+                : "flex items-center  text-[16px] gap-3 font-semibold"
+            }
+          >
+            <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
+            Notifiction text-1
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/logout"
+            className={({ isActive }) =>
+              isActive
+                ? "flex items-center  text-[16px] gap-3 font-semibold text-blue-600"
+                : "flex items-center  text-[16px] gap-3 font-semibold"
+            }
+          >
+            <IoNotificationsOutline className="text-red-600"></IoNotificationsOutline>{" "}
+            Notifiction text-1
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Notifiction;
